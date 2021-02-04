f619b6318a95660f7789ee34696cd5d26f615bbb
nathan sheryak <nathanraysheryak@gmail.com>
2021-02-04 07:22:24 -0500

    firebase init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import { createApp } from 'vue';
+import * as firebase from 'firebase';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGEINGSENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APPID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENTID,
+};
+
+firebase.default.initializeApp(firebaseConfig);
 
 createApp(App).use(store).use(router).mount('#app');
